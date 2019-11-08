12fe56741ac3cc1a5efabd72b4921a8d8d097219
harounchahed <haroun.chd@gmail.com>
2019-11-08 14:55:46 +0800

    added activity for when photo is clicked on catsfeed

--- app/src/main/java/com/ysc3237/snapcat/ui/Catsfeed/myAdapter.java
+++ app/src/main/java/com/ysc3237/snapcat/ui/Catsfeed/myAdapter.java
@@ -1,6 +1,7 @@
 package com.ysc3237.snapcat.ui.Catsfeed;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,9 +32,20 @@ public class myAdapter extends RecyclerView.Adapter<CatViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(CatViewHolder holder, int position) {
+    public void onBindViewHolder(final CatViewHolder holder, int position) {
         holder.mImage.setImageResource(mCatList.get(position).getCatImage());
         holder.mTitle.setText(mCatList.get(position).getCatName());
+        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(mContext, DetailActivity.class);
+                mIntent.putExtra("Title", mCatList.get(holder.getAdapterPosition()).getCatName());
+                mIntent.putExtra("Description", mCatList.get(holder.getAdapterPosition()).getCatDescription());
+                mIntent.putExtra("Image", mCatList.get(holder.getAdapterPosition()).getCatImage());
+                mContext.startActivity(mIntent);
+
+            }
+        });
 
     }
 
@@ -47,7 +59,7 @@ class CatViewHolder extends RecyclerView.ViewHolder {
 
     ImageView mImage;
     TextView mTitle;
-    //CardView mCardView;
+    CardView mCardView;
 
 
     CatViewHolder(View itemView) {
@@ -55,7 +67,7 @@ class CatViewHolder extends RecyclerView.ViewHolder {
 
         mImage =  itemView.findViewById(R.id.ivImage);
         mTitle =  itemView.findViewById(R.id.tvTitle);
-        //mCardView = itemView.findViewById(R.id.cardview);
+        mCardView = itemView.findViewById(R.id.cardview);
 
     }
 }
